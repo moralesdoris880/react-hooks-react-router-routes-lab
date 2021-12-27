11a2e3baebdfde30e55e9ce13b21852da77e5041
Doris Morales <moralesdoris880@gmail.com>
2021-12-27 14:09:55 -0500

    Completed assignment just need to pass tests

--- src/components/Directors.js
+++ src/components/Directors.js
@@ -2,7 +2,23 @@ import React from "react";
 import { directors } from "../data";
 
 function Directors() {
-  return <div>{/*{code here}*/}</div>;
+  return <div>
+    <h1>Directors Page</h1>
+    <div>
+      {directors.map((director)=>{
+        return (
+          <div>
+            <h1>{director.name}</h1>
+            <ul>{director.movies.map((movie)=>{
+              return (
+                <li>{movie}</li>
+              )
+            })}</ul>
+          </div>
+        )
+      })}
+    </div>
+  </div>;
 }
 
 export default Directors;
